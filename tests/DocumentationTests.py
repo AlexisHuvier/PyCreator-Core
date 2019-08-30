3f8f815e68c8ac89ec1ae8cef1909950b1890ac6
LavaPower <lavapower84@gmail.com>
2019-08-30 14:42:46 +0200

    Move DocumentationTests in a new file

---
+++ tests/DocumentationTests.py
@@ -0,0 +1,33 @@
+import unittest
+import pycreator_core
+
+
+class DocumentationTests(unittest.TestCase):
+    def test_getdoc(self):
+        self.assertEqual(pycreator_core.get_doc("compile"), "https://docs.python.org/3/search.html?q=compile")
+
+    def test_gethemlp(self):
+        self.assertEqual(pycreator_core.get_help("WTF"), "No Python documentation found for 'WTF'.\n"
+                                                         "Use help() to get the interactive help utility.\n"
+                                                         "Use help(str) for help on the str class.\n\n")
+        self.assertEqual(pycreator_core.get_help("pycreator_core"), """Help on package pycreator_core:
+
+NAME
+    pycreator_core
+
+PACKAGE CONTENTS
+    autocompletion
+    config
+    documentation
+    execute
+    history
+    utils
+
+VERSION
+    0.0.1
+
+FILE
+    d:\programmation\python\projet\pycreator\pycreator-core\pycreator_core\__init__.py
+
+
+""")
--- tests/OthersTests.py
+++ tests/OthersTests.py
@@ -7,32 +7,6 @@ class InformationsTests(unittest.TestCase):
         self.assertEqual(pycreator_core.__version__, "0.0.1")
 
 
-class DocumentationTests(unittest.TestCase):
-    def test_getdoc(self):
-        self.assertEqual(pycreator_core.get_doc("compile"), "https://docs.python.org/3/search.html?q=compile")
-
-    def test_gethemlp(self):
-        self.assertEqual(pycreator_core.get_help("WTF"), "No Python documentation found for 'WTF'.\n"
-                                                         "Use help() to get the interactive help utility.\n"
-                                                         "Use help(str) for help on the str class.\n\n")
-        self.assertEqual(pycreator_core.get_help("pycreator_core"), """Help on package pycreator_core:
-
-NAME
-    pycreator_core
-
-PACKAGE CONTENTS
-    documentation
-    execute
-    utils
-
-VERSION
-    0.0.1
-
-FILE
-    d:\programmation\python\projet\pycreator\pycreator-core\pycreator_core\__init__.py
-
-
-""")
 class ConfigTests(unittest.TestCase):
     def setUp(self) -> None:
         self.config = pycreator_core.Config()
