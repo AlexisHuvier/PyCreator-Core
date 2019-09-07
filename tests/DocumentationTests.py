2b50fbb0059ab8732582a27cca06adcc616e7d5a
LavaPower <lavapower84@gmail.com>
2019-09-08 01:55:43 +0200

    Unit Tests: Update DocumentationTests

--- tests/DocumentationTests.py
+++ tests/DocumentationTests.py
@@ -2,15 +2,26 @@ import unittest
 import pycreator_core
 
 
+class FakeStdout:
+    def __init__(self):
+        self.contenu = ""
+
+    def write(self, data):
+        self.contenu += data
+
+
 class DocumentationTests(unittest.TestCase):
     def test_getdoc(self):
         self.assertEqual(pycreator_core.get_doc("compile"), "https://docs.python.org/3/search.html?q=compile")
 
     def test_gethemlp(self):
-        self.assertEqual(pycreator_core.get_help("WTF"), "No Python documentation found for 'WTF'.\n"
-                                                         "Use help() to get the interactive help utility.\n"
-                                                         "Use help(str) for help on the str class.\n\n")
-        self.assertEqual(pycreator_core.get_help("pycreator_core"), """Help on package pycreator_core:
+        std = FakeStdout()
+        pycreator_core.get_help("WTF", std.write)
+        self.assertEqual(std.contenu, "No Python documentation found for 'WTF'.\nUse help() to get the interactive "
+                                      "help utility.\nUse help(str) for help on the str class.\n\n")
+        std.contenu = ""
+        pycreator_core.get_help("pycreator_core", std.write)
+        self.assertEqual(std.contenu, """Help on package pycreator_core:
 
 NAME
     pycreator_core
