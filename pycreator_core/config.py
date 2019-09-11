e06b82c4de4931986dcffdf5d502423e3c39e3d9
LavaPower <lavapower84@gmail.com>
2019-09-11 17:04:45 +0200

    Update Config

--- pycreator_core/config.py
+++ pycreator_core/config.py
@@ -8,10 +8,29 @@ class Config:
             self.config = json.load(f)
 
     def get(self, key, default):
-        return self.config.get(key, default)
+        l = key.split(".")
+        if len(l) == 1:
+            return self.config.get(key, default)
+        else:
+            value = self.config
+            for i in l:
+                if i != l[-1]:
+                    value = value[i]
+                else:
+                    value = value.get(i, default)
+            return value
 
     def set(self, key, value):
-        self.config[key] = value
+        l = key.split(".")
+        if len(l) == 1:
+            self.config[key] = value
+        else:
+            tochange = self.config
+            for i in l:
+                if i != l[-1]:
+                    tochange = tochange[i]
+                else:
+                    tochange[i] = value
 
     def save(self):
         with open(os.path.join(os.path.abspath(os.path.split(__file__)[0]), "config.json"), "w") as f:
